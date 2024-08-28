cd9ed16abcd4cd7b8b6c96709e11fddcb1cb9726
Luis gonzalesrubio <lgsantiago07@gmail.com>
2024-08-28 12:34:34 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carritoventasvirtual',
+  webDir: 'www'
+};
+
+export default config;
